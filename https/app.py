aada424cadc84591aa62c2b3718deccca96a56b5
root <github@liforra.aleeas.com>
2024-08-01 15:02:04 +0000

    moving home to contact

--- https/app.py
+++ https/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, request
+from flask import Flask, send_from_directory, request, abort, render_template
 import os
 
 app = Flask(__name__, static_folder=os.getcwd())
@@ -19,7 +19,24 @@ def serve_index():
 
 @app.route('/<path:path>')
 def serve_file(path):
-    return send_from_directory(app.static_folder, path)
+    # Check if the path is a directory and if it contains an index.html file
+    full_path = os.path.join(app.static_folder, path)
+    if os.path.isdir(full_path):
+        index_file = os.path.join(full_path, 'index.html')
+        if os.path.exists(index_file):
+            return send_from_directory(full_path, 'index.html')
+    
+    # If it's not a directory or doesn't contain index.html, serve the file directly
+    if os.path.exists(full_path):
+        return send_from_directory(app.static_folder, path)
+    
+    # If the file doesn't exist, return a 404
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # Serve the custom 404 page
+    return send_from_directory(app.static_folder, '404.html'), 404
 
 if __name__ == '__main__':
     app.run()
